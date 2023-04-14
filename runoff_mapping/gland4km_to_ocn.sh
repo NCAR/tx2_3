45249d2b245b8e1b253f2a8a62f171d8cad55d23
Gustavo Marques <gmarques@ucar.edu>
2023-04-14 13:45:10 -0600

    Adding runoff files

---
+++ runoff_mapping/gland4km_to_ocn.sh
@@ -0,0 +1,3 @@
+source /glade/work/gmarques/cesm.sandboxes/cesm2_3_beta08_carib/cime/tools/mapping/gen_mapping_files/runoff_to_ocn/src/.env_mach_specific.sh
+export TMPDIR=/glade/scratch/gmarques
+/glade/work/gmarques/cesm.sandboxes/cesm2_3_beta08_carib/cime/tools/mapping/gen_mapping_files/runoff_to_ocn/runoff_map < map_greenland_4km_to_tx2_3.nml
